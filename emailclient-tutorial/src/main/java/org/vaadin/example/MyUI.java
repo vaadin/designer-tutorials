d500d43005600ba0b1b018341c3a18e7c30a82de
Johannes Häyry <johannes.hayry@vaadin.com>
2015-10-22 17:27:32 +0300

    Work-in-progress

---
+++ emailclient-tutorial/src/main/java/org/vaadin/example/MyUI.java
@@ -0,0 +1,26 @@
+package org.vaadin.example;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+/**
+ *
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setContent(new ApplicationDesign());
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
